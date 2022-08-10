70a20f6f3bca434d86bd351ef0248ed673957b52
mandy85283 <mandy@DESKTOP-A9PRCRB>
2022-08-09 18:13:47 -0700

    update

--- DiceRoller/DiceRoller/Models/Die.cs
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -6,5 +6,24 @@ namespace DiceRoller.Models
 {
     public class Die
     {
+        // name,type, or colour of the die
+        public String Name { get; set; }
+        //how many sides are the die
+        public int NumSides { get; set; }
+        //which number is currently up
+        public int CurrentSide { get; set; }
+
+        public Die()
+        {
+            NumSides = 6;
+            Name = "d6";
+            CurrentSide = 2;
+
+        }
+        public void Roll()
+        {
+           // Random random = new Random();
+            //CurrentSide = random.Next(NumSides) + 1;
+        }
     }
 }
--- DieTests/UnitTest1.cs
+++ DieTests/UnitTest1.cs
@@ -7,11 +7,37 @@ namespace DieTests
     [TestClass]
     public class UnitTest1
     {
+        Die def = new Die();
         [TestMethod]
         public void DieNotNull()
         {
-            Die d = new Die();
-            d.Should().NotBeNull();
+            def.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void DieHasAllDefaultValues()
+        {
+            /*
+             Default values should be:
+            name: d6
+            numsides: 6*/
+
+            def.Name.Should().Be("d6");
+            def.NumSides.Should().Be(6);
+            def.CurrentSide.Should().BeInRange(1, 6);
+
+        }
+
+        [TestMethod]
+        public void RollSetsSideCorrectly()
+        {
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                def.Roll();
+                def.CurrentSide.Should().BeInRange(1, 6);
+            }
         }
     }
 }
