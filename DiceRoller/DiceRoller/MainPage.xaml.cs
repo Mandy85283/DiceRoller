e1286b016944e9edeaeb70f1622eaf6e2d773f45
mandy85283 <mandy@DESKTOP-A9PRCRB>
2022-08-13 16:20:23 -0700

    in this Commit i created 4 more test methods named LabelResult1IsDisplayed, LabelResult2IsDisplayed, RollOnceIstapped and RollTwiceIsTapped where it test both the results are displayed or not and Roll id pressed once ot twice .

--- DiceRoller/DiceRoller/MainPage.xaml.cs
+++ DiceRoller/DiceRoller/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using DiceRoller.Models;
 
 namespace DiceRoller
 {
@@ -14,15 +15,127 @@ namespace DiceRoller
         {
             InitializeComponent();
         }
-        public void DisplayOne()
+        public void DisplayOne(object sender, CheckedChangedEventArgs e)
         {
-            //display 1 result
-            //hide label 2
+            RadioButton button = sender as RadioButton;
+            var i = button.Content;
+            i.ToString();
+
+
+
         }
-        public void DisplayTwo()
+
+        public void DisplayTwo(object sender, EventArgs args)
         {
-            //display 2 result
-            //show label 2
+            Result2.IsVisible = true;
+            if (d4.IsChecked)
+            {
+                Die chosenDice1 = new Die(4);
+                Die chosenDice2 = new Die(4);
+                string result1 = chosenDice1.CurrentSide.ToString();
+                string result2 = chosenDice2.CurrentSide.ToString();
+                Result1.Text = "Die 1: " + result1;
+                Result2.Text = "Die 2: " + result2;
+            }
+            else if (d6.IsChecked)
+            {
+                Die chosenDice1 = new Die();
+                Die chosenDice2 = new Die();
+                string result1 = chosenDice1.CurrentSide.ToString();
+                string result2 = chosenDice2.CurrentSide.ToString();
+                Result1.Text = "Die 1: " + result1;
+                Result2.Text = "Die 2: " + result2;
+            }
+            else if (d8.IsChecked)
+            {
+                Die chosenDice1 = new Die(8);
+                Die chosenDice2 = new Die(8);
+                string result1 = chosenDice1.CurrentSide.ToString();
+                string result2 = chosenDice2.CurrentSide.ToString();
+                Result1.Text = "Die 1: " + result1;
+                Result2.Text = "Die 2: " + result2;
+            }
+            else if (d10.IsChecked)
+            {
+                Die chosenDice1 = new Die(10);
+                Die chosenDice2 = new Die(10);
+                string result1 = chosenDice1.CurrentSide.ToString();
+                string result2 = chosenDice2.CurrentSide.ToString();
+                Result1.Text = "Die 1: " + result1;
+                Result2.Text = "Die 2: " + result2;
+            }
+            else if (d12.IsChecked)
+            {
+                Die chosenDice1 = new Die(12);
+                Die chosenDice2 = new Die(12);
+                string result1 = chosenDice1.CurrentSide.ToString();
+                string result2 = chosenDice2.CurrentSide.ToString();
+                Result1.Text = "Die 1: " + result1;
+                Result2.Text = "Die 2: " + result2;
+            }
+            else if (d20.IsChecked)
+            {
+                Die chosenDice1 = new Die(20);
+                Die chosenDice2 = new Die(20);
+                string result1 = chosenDice1.CurrentSide.ToString();
+                string result2 = chosenDice2.CurrentSide.ToString();
+                Result1.Text = "Die 1: " + result1;
+                Result2.Text = "Die 2: " + result2;
+            }
+            else
+            {
+                Result1.Text = "Select a die to roll";
+            }
+
+
+        }
+
+        private void DisplayOne(object sender, EventArgs args)
+        {
+            Result2.IsVisible = false;
+            if (d4.IsChecked)
+            {
+                Die chosenDice = new Die(4);
+                string result = chosenDice.CurrentSide.ToString();
+                Result1.Text = result;
+            }
+            else if (d6.IsChecked)
+            {
+                Die chosenDice = new Die();
+                string result = chosenDice.CurrentSide.ToString();
+                Result1.Text = result;
+            }
+            else if (d8.IsChecked)
+            {
+                Die chosenDice = new Die(8);
+                string result = chosenDice.CurrentSide.ToString();
+                Result1.Text = result;
+            }
+            else if (d10.IsChecked)
+            {
+                Die chosenDice = new Die(10);
+                string result = chosenDice.CurrentSide.ToString();
+                Result1.Text = result;
+            }
+            else if (d12.IsChecked)
+            {
+                Die chosenDice = new Die(12);
+                string result = chosenDice.CurrentSide.ToString();
+                Result1.Text = result;
+            }
+            else if (d20.IsChecked)
+            {
+                Die chosenDice = new Die(20);
+                string result = chosenDice.CurrentSide.ToString();
+                Result1.Text = result;
+            }
+            else
+            {
+                Result1.Text = "Select a die to roll";
+            }
+
+
+
         }
     }
 }
--- UITests/Tests.cs
+++ UITests/Tests.cs
@@ -78,5 +78,57 @@ namespace UITests
             AppResult[] results = app.Query(c => c.Property("text").Contains("Display "));
             Assert.IsTrue(results.Length == 2);
         }
+
+        [Test]
+        [Category("UI")]
+        public void LabelResult1IsDisplayed()
+        {
+            app.Tap(c => c.Property("text").Contains("Display one result"));
+
+            AppResult[] results = app.Query(c => c.Property("text").Contains("roll"));
+
+            Assert.IsTrue(results.Length > 0);
+
+        }
+
+        [Test]
+        [Category("UI")]
+        public void LabelResult2IsDisplayed()
+        {
+            app.Tap(c => c.Marked("d4"));
+            app.Tap(c => c.Property("text").Contains("Display two results"));
+
+            AppResult[] results = app.Query(c => c.Property("text").Contains("Die 2"));
+
+            Assert.IsTrue(results.Length > 0);
+
+        }
+
+        [Test]
+        public void RollOnceIstapped()
+        {
+            app.Tap(c => c.Marked("Roll Once"));
+
+            string resultOneText = app.Query(c => c.Id("ResultOne"))[0].Text;
+            int resultVal = int.Parse(resultOneText);
+
+            Assert.NotNull(resultVal);
+        }
+
+        [Test]
+        public void RollTwiceIsTapped()
+        {
+            app.Tap(c => c.Marked("Roll Twice"));
+
+            string resultOneText = app.Query(c => c.Id("resultOne"))[0].Text;
+            int resultVal = int.Parse(resultOneText);
+
+            Assert.NotNull(resultVal);
+
+            string resultTwoText = app.Query(c => c.Id("resultTwice"))[0].Text;
+            int resultValTwo = int.Parse(resultTwoText);
+
+            Assert.NotNull(resultValTwo);
+        }
     }
 }
